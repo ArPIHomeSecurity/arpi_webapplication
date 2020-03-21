1e293cd5dd0128812ed53acc78edf550d757e42d
gkovacs81 <gkovacs81@gmail.com>
2020-03-21 18:59:43 +0100

    Upgrade material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -48,27 +48,25 @@ import { routing } from './app.routing';
 import { PageNotFoundComponent } from './page-not-found.component';
 
 // material components
-import {
-  MatInputModule,
-  MatToolbarModule,
-  MatSidenavModule,
-  MatListModule,
-  MatButtonModule,
-  MatIconModule,
-  MatCardModule,
-  MatSlideToggleModule,
-  MatButtonToggleModule,
-  MatSnackBarModule,
-  MatExpansionModule,
-  MatTableModule,
-  MatChipsModule,
-  MatSelectModule,
-  MatRadioModule,
-  MatCheckboxModule,
-  MatDialogModule,
-  MatProgressSpinnerModule,
-  MatTooltipModule
-} from '@angular/material';
+import { MatButtonModule } from '@angular/material/button';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { MatCardModule } from '@angular/material/card';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatChipsModule } from '@angular/material/chips';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatListModule } from '@angular/material/list';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatSelectModule } from '@angular/material/select';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatTableModule } from '@angular/material/table';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatTooltipModule } from '@angular/material/tooltip';
 
 
 @NgModule({
