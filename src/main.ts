1c3a58af36f9ace7f895c5eefcbdb8ea3a598886
gkovacs81 <gkovacs81@gmail.com>
2022-09-20 06:39:58 +0200

    Fix locale redirect

--- src/main.ts
+++ src/main.ts
@@ -6,17 +6,31 @@ import { environment } from './environments/environment';
 
 
 let locale = localStorage.getItem('localeId');
-// console.log('Selected language: ', locale);
+console.log('Selected language: ', locale);
 if (locale === null) {
   locale = 'en';
 }
 
-// console.log('Current location: ', location.pathname);
-if (location.pathname.startsWith('/' + locale)) {
-  // console.log('Correct locale, no need to redirect!');
-} else if (locale !== 'en') {
-  // console.log('Other locale is selected => redirect', locale);
-  location.href = ('/' + locale + location.pathname).replace(/\/$/, '');
+console.log('Current location: ', location.pathname);
+var localeRegex = new RegExp('^/([a-z]{2})/');
+var matches = localeRegex.exec(location.pathname);
+
+console.log('Current locale: ', matches === null ? 'en' : matches[1]);
+
+if ((matches === null && locale === 'en') || (matches !== null && locale === matches[1])) {
+  console.log('Correct locale, no need to redirect!');
+}
+else if (matches === null && locale != 'en') {
+  // redirect to locale (EN => X)
+  const newPath = locale + location.pathname;
+  console.log('Redirect to ' + newPath);
+  location.pathname = newPath;
+}
+else if (matches !== null && locale == 'en') {
+  // redirect to locale (X => EN)
+  const newPath = location.pathname.replace(matches[0], '/');
+  console.log('Redirect to ' + newPath);
+  location.pathname = newPath;
 }
 
 if (environment.production) {
