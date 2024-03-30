e9b426f6f955817a90dcd703b64a408e2c48e938
gkovacs81 <gkovacs81@gmail.com>
2024-03-30 19:38:22 +0100

    Fix authguard

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -1,9 +1,13 @@
 import { inject } from '@angular/core';
-import { CanActivateFn } from '@angular/router';
+import { CanActivateFn, Router } from '@angular/router';
 
 import { AUTHENTICATION_SERVICE } from '../tokens';
 
 export const AuthGuard: CanActivateFn = () => {
   const authService = inject(AUTHENTICATION_SERVICE);
-  return authService.isLoggedIn();
+  if (authService.isLoggedIn()) {
+    return true;
+  }
+
+  inject(Router).navigate(['/login']);
 };
